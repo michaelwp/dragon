a2c017ed9738792c733a896f452a57ed6464a971
MIchael <michael.wenceslaus@kanggo.id>
2021-09-05 22:05:57 +0700

    feature body

--- dragon/router.go
+++ dragon/router.go
@@ -1,10 +1,67 @@
 package dragon
 
 import (
+	"log"
 	"net/http"
 )
 
 var routers = make([]*router, 0)
+
+func NewRouter() Router {
+	return &router{}
+}
+
+func logging(req *http.Request) {
+	log.Printf("%s - %s %s", req.RemoteAddr, req.Method, req.URL.Path)
+}
+
+func (r *router) setupDragonParameter(rw http.ResponseWriter, req *http.Request) *Dragon {
+	return &Dragon{
+		ResponseWriter: rw,
+		Request:        req,
+		Context:        req.Context(),
+		Params:         r.mapParamsUrl(),
+		Headers:        req.Header,
+		Path:           req.URL.Path,
+		Query:          req.URL.Query(),
+		RemoteAddress:  req.RemoteAddr,
+	}
+
+}
+
+func (r *router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	// validating http method requested
+	hh, rr, err := isPathExist(rw, req)
+	if err != nil {
+		return
+	}
+
+	// checking middleware
+	err = middlewareChecking(rw, req)
+	if err != nil {
+		return
+	}
+
+	// setup dragon parameter
+	dragon := rr.setupDragonParameter(rw, req)
+
+	// update handler http response writer & request
+	hh.Handler(dragon)
+
+	// simple request logging
+	logging(req)
+}
+
+func (r *router) Run(address string) error {
+	log.Printf("Server listen and serve on %s", address)
+	err := http.ListenAndServe(address, r)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (r router) Group(path string) router {
 	r.RouterGroup = r.RouterGroup + path
