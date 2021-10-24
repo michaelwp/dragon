7e1970b8ccb1cda33084a29ead3ff76e29f9a8ad
michaelwp <michael.wenceslaus@gmail.com>
2021-10-24 16:32:08 +0700

    #36 - add feature response html

--- response.go
+++ response.go
@@ -3,6 +3,7 @@ package dragon
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 )
 
 func (d *Dragon) ResponseJSON(statusCode int, v interface{}) error {
@@ -20,6 +21,17 @@ func (d *Dragon) ResponseXML(statusCode int, v interface{}) error {
 	d.ResponseHeader.Set("Content-Type", "application/xml")
 	d.ResponseStatus(statusCode)
 	err := xml.NewEncoder(d.ResponseWriter).Encode(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Dragon) ResponseHTML(statusCode int, v interface{}) error {
+	d.ResponseHeader.Set("Content-Type", "text/html; charset=utf-8")
+	d.ResponseStatus(statusCode)
+	_, err := fmt.Fprintf(d.ResponseWriter, v.(string))
 	if err != nil {
 		return err
 	}
